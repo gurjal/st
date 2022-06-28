6d4e0033e4ace2f55f42f055625de1349ebb7177
gurjal <shaanraya135@gmail.com>
2022-06-27 18:00:45 -0700

    default config changes and added themes directory

---
+++ themes/embark.h
@@ -0,0 +1,38 @@
+/*
+ * st/themes/embark.h
+ *
+ * black
+ * red
+ * green
+ * yellow
+ * blue
+ * purple
+ * cyan
+ * white
+ *
+ */
+
+static const char *colorname[] = {
+  [0]   = "#1E1C31",
+  [1]   = "#F0719B",
+  [2]   = "#A1EFD3",
+  [3]   = "#FFE9AA",
+  [4]   = "#57C7FF",
+  [5]   = "#C792EA",
+  [6]   = "#87DFEB",
+  [7]   = "#F8F8F2",
+  [8]   = "#585273",
+  [9]   = "#F02E6E",
+  [10]  = "#2CE592",
+  [11]  = "#FFB378",
+  [12]  = "#1DA0E2",
+  [13]  = "#A742EA",
+  [14]  = "#63F2F1",
+  [15]  = "#A6B3CC",
+  [255] = 0,
+};
+
+unsigned int defaultfg = 7;
+unsigned int defaultbg = 0;
+unsigned int defaultcs = 13;
+static unsigned int defaultrcs = 0;
