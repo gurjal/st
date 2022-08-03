d30ec75cdd5ced8a375e9ea32d861dbfd47f3034
gurjal <shaanraya135@gmail.com>
2022-08-03 08:12:56 -0700

    new themes

---
+++ themes/seoul256.h
@@ -0,0 +1,39 @@
+/*
+ * st/themes/seoul256.h
+ *
+ * black
+ * red
+ * green
+ * yellow
+ * blue
+ * purple
+ * cyan
+ * white
+ *
+ */
+
+static const char *colorname[] = {
+  [0]   = "#4e4e4e",
+  [1]   = "#d68787",
+  [2]   = "#5f865f",
+  [3]   = "#d8af5f",
+  [4]   = "#85add4",
+  [5]   = "#d7afaf",
+  [6]   = "#87afaf",
+  [7]   = "#d0d0d0",
+  [8]   = "#626262",
+  [9]   = "#d75f87",
+  [10]  = "#87af87",
+  [11]  = "#ffd787",
+  [12]  = "#add4fb",
+  [13]  = "#ffafaf",
+  [14]  = "#87d7d7",
+  [15]  = "#e4e4e4",
+  [255] = 0,
+  [256] = "#3a3a3a",
+};
+
+unsigned int defaultfg = 7;
+unsigned int defaultbg = 256;
+unsigned int defaultcs = 13;
+static unsigned int defaultrcs = 0;
