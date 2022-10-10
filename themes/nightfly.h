b56ba8d2ec1fa7d5d9ac7cc53240d5c41250b105
gurjal <shaanraya135@gmail.com>
2022-10-10 12:44:21 -0700

    new nightfly theme added

---
+++ themes/nightfly.h
@@ -0,0 +1,42 @@
+/*
+ * st/themes/nightfly.h
+ *
+ * black
+ * red
+ * green
+ * yellow
+ * blue
+ * purple
+ * cyan
+ * white
+ *
+ */
+
+static const char *colorname[] = {
+  [0]   = "#1d3b53",
+  [1]   = "#fc514e",
+  [2]   = "#a1cd5e",
+  [3]   = "#e3d18a",
+  [4]   = "#82aaff",
+  [5]   = "#c792ea",
+  [6]   = "#7fdbca",
+  [7]   = "#a1aab8",
+  [8]   = "#7c8f8f",
+  [9]   = "#ff5874",
+  [10]  = "#21c7a8",
+  [11]  = "#ecc48d",
+  [12]  = "#82aaff",
+  [13]  = "#ae81ff",
+  [14]  = "#7fdbca",
+  [15]  = "#d6deeb",
+  [255] = 0,
+  [256] = "#011627",
+  [257] = "#acb4c2",
+  [258] = "#9ca1aa",
+  [259] = "#b2ceee",
+};
+
+unsigned int defaultbg = 256;
+unsigned int defaultfg = 257;
+unsigned int defaultcs = 258;
+static unsigned int defaultrcs = 259;
