c8511f78774f1be09e0a1ba65f0d15d1d0bba92d
gurjal <shaanraya135@gmail.com>
2022-11-24 20:45:41 -0800

    new theme tundra

---
+++ themes/tundra.h
@@ -0,0 +1,40 @@
+/*
+ * st/themes/tundra.h
+ *
+ * black
+ * red
+ * green
+ * yellow
+ * blue
+ * purple
+ * cyan
+ * white
+ *
+ */
+
+static const char *colorname[] = {
+  [0]   = "#6B7280",
+  [1]   = "#FCA5A5",
+  [2]   = "#B1E3AD",
+  [3]   = "#FBC19D",
+  [4]   = "#BAE6FD",
+  [5]   = "#DDD6FE",
+  [6]   = "#BAE6FD",
+  [7]   = "#D1D5DB",
+  [8]   = "#6B7280",
+  [9]   = "#FCA5A5",
+  [10]  = "#B1E3AD",
+  [11]  = "#FBC19D",
+  [12]  = "#BAE6FD",
+  [13]  = "#DDD6FE",
+  [14]  = "#BAE6FD",
+  [15]  = "#D1D5DB",
+  [255] = 0,
+  [256] = "#D1D5DB", // foreground
+  [257] = "#111827", // background
+};
+
+unsigned int defaultfg = 256;
+unsigned int defaultbg = 257;
+unsigned int defaultcs = 256;
+static unsigned int defaultrcs = 257;
